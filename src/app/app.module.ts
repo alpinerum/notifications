2cdd1779cced3dce34902fb59b85b639dbae0fb9
alpinerum <35735448+alpinerum@users.noreply.github.com>
2024-06-25 22:14:35 -0400

    created notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { LocalNotifications } from '@awesome-cordova-plugins/local-notifications/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
@@ -10,7 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [LocalNotifications, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LocalNotifications, ELocalNotificationTriggerUnit } from '@awesome-cordova-plugins/local-notifications/ngx';
+import { Platform, NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,60 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private plt: Platform, private localNotifications: LocalNotifications, private alertCtrl: AlertController) {
+    this.plt.ready().then(() => {
+      this.localNotifications.on('click').subscribe(res => {
+        let msg = res.data ? res.data.info : '';
+        this.displayanAlert(res.title, res.text, msg);
+      });
 
+      this.localNotifications.on('trigger').subscribe(res => {
+        let msg = res.data ? res.data.info : '';
+        this.displayanAlert(res.title, res.text, msg);
+      });
+    });
+  
+  }
+
+  getAftertenseconds() {
+    this.localNotifications.schedule({
+      id: 1,
+      title: 'Hello',
+      text: 'Notification',
+      data: { info: 'This message appears when you open it' },
+      trigger: { in: 10, unit: ELocalNotificationTriggerUnit.SECOND},
+      foreground: true
+    });
+  }
+
+  getRecurring() {
+    this.localNotifications.schedule({
+      id: 2,
+      title: 'You will see me every 50 seconds',
+      text: 'Hello, see you in 50 seconds',
+      trigger: { in: 50, unit: ELocalNotificationTriggerUnit.SECOND},
+      foreground: true
+    });
+  }
+
+  getDaily() {
+    this.localNotifications.schedule({
+      id: 3,
+      title: 'Daily Notification',
+      text: 'Time to wake up!',
+      trigger: { every: {hour: 8, minute: 0}},
+      foreground: true
+    });
+  }
+
+  displayanAlert(header : any, sub : any, msg : any) {
+    this.alertCtrl.create({
+      header: header,
+      subHeader: sub,
+      message: msg,
+      buttons: ['Ok']
+    }).then(alert => alert.present());
+  }
 }
+
+
